38e45e8b394602d2395ad43d56482cf27d0e0d35
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-04-27 15:46:26 +0200

    CameraControllerv2

---
+++ GraRTS/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform cameraTransform;
+
+    public float normalSpeed;
+    public float fastSpeed;
+    public float movementSpeed;
+    public float movementTime;
+    public float rotationAmount;
+    public float zoomSensivity;
+    public Vector3 zoomAmount;
+    
+    public Vector3 newPosition;
+    public Vector3 newZoom;
+    public Quaternion newRotation;
+    public Vector3 rotateStartPosition;
+    public Vector3 rotateCurrentPosition;
+    private void Start()
+    {
+        newPosition = transform.position;
+        newRotation = transform.rotation;
+        newZoom = cameraTransform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();
+        HandleMouseInput();
+    }
+
+    void HandleMovementInput()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = fastSpeed;
+        }
+        else
+        {
+            movementSpeed = normalSpeed;
+        }
+
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.right * -movementSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
+        if(Input.GetKey(KeyCode.R))
+        {
+            newZoom += zoomAmount;
+        }
+        if(Input.GetKey(KeyCode.F))
+        {
+            newZoom -= zoomAmount;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+    }
+
+    void HandleMouseInput()
+    {
+        if(Input.mouseScrollDelta.y != 0 )
+        {
+            newZoom += Input.mouseScrollDelta.y * zoomAmount * zoomSensivity;
+        }
+
+        if(Input.GetMouseButtonDown(2))
+        {
+            rotateStartPosition = Input.mousePosition;
+        }
+        if(Input.GetMouseButton(2))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+            Vector3 diffrence = rotateStartPosition - rotateCurrentPosition;
+
+            rotateStartPosition = rotateCurrentPosition;
+
+            newRotation *= Quaternion.Euler(Vector3.up * (-diffrence.x / 5f));
+        }
+
+    }
+}
