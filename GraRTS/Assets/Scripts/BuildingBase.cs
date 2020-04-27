f4b5489a8b2bf69431aca4d89c32084464e86253
piotrwojtaszek <piotrwojtaszek@users.noreply.github.com>
2020-04-27 19:07:53 +0200

    ObjectsPlacement

---
+++ GraRTS/Assets/Scripts/BuildingBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseOfBuilding",menuName = "MyAssets/Budildings")]
+public class BuildingBase : ScriptableObject
+{
+    public float maxHealth;
+    public string name;
+    public GameObject model;
+
+
+    public void Destroy(GameObject obj)
+    {
+        Destroy(obj);
+    }
+}
---
+++ GraRTS/Assets/Scripts/Camera/GroundPlacementController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPlacementController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject placeableObjectPrefab;
+    [SerializeField]
+    private KeyCode newObjectHotkey = KeyCode.A;
+    private GameObject currentPlaceableObject;
+    private float mouseWheelRotation;
+    [SerializeField]
+    private LayerMask collisionMask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleNewObjectHotkey();  
+        if(currentPlaceableObject !=null)
+        {
+            MoveCurrentPlaceableObjectToMouse();
+            RotateFromMouseWheel();
+            RealseIfClicked();
+        }
+    }
+
+    private void RealseIfClicked()
+    {
+       
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject.GetComponent<ObjectControllerBuildMode>().ChangeLayer();
+            currentPlaceableObject = null;
+
+        }
+    }
+
+    private void RotateFromMouseWheel()
+    {
+        mouseWheelRotation += Input.mouseScrollDelta.y;
+        currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+    }
+
+    private void MoveCurrentPlaceableObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray,out hitInfo,Mathf.Infinity,collisionMask))
+        {
+
+            currentPlaceableObject.transform.position = hitInfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            Collider[] hitColliders = Physics.OverlapSphere(currentPlaceableObject.transform.position, 1f,collisionMask);
+            if(hitColliders.Length>1)
+            {
+                currentPlaceableObject.GetComponent<ObjectControllerBuildMode>().UnableToPlace();
+
+                foreach (Collider col in hitColliders)
+                {
+                    Debug.Log(col.name);
+                }
+            }
+            else
+            {
+                currentPlaceableObject.GetComponent<ObjectControllerBuildMode>().AbleToPlace();
+            }
+        }
+    }
+
+    private void HandleNewObjectHotkey()
+    {
+        if(Input.GetKeyDown(newObjectHotkey))
+        {
+            if(currentPlaceableObject==null)
+            {
+                currentPlaceableObject = Instantiate(placeableObjectPrefab);
+            }
+            else
+            {
+                Destroy(currentPlaceableObject);
+            }
+        }
+    }
+}
